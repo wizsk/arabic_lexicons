fb96cb379a4c127ec6e44fbf15a1d231a7b65978
Sakib <sakibul706@gmail.com>
2026-07-07 15:52:41 +0600

    clean

--- lib/multi_selection.dart
+++ lib/multi_selection.dart
@@ -1,7 +1,5 @@
 import 'package:ara_dict/main_widgets.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
 class SelectionController<T> {
   final selected = <T>{};

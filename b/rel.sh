4a818d86f20a261c172d73cf1458121075f8a87f
Sakib <sakibul706@gmail.com>
2026-07-09 10:32:37 +0600

    rel

--- b/rel.sh
+++ b/rel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REQUIRED_FLUTTER_VERSION="3.44.4"
+REQUIRED_FLUTTER_VERSION="3.44.5"
 
 # Check if flutter exists
 if ! command -v flutter &>/dev/null; then
@@ -18,44 +18,93 @@ fi
 
 echo "Flutter $FLUTTER_VERSION found"
 
-
 source ./b/common.sh
 source ./b/confirm.sh
 
-echo "Starting build..."
+BUILD_DIR="/tmp/build"
+OUT_DIR="build-fdroid"
 
-set -ex
+version=$ver
 
-flutter build apk --release --split-per-abi \
-  --target-platform="android-arm" \
-  --dart-define=APP_VERSION="$ver" \
-  --dart-define=GIT_COMMIT="$gc"
+echo "Version: $version"
 
-flutter build apk --release --split-per-abi \
-  --target-platform="android-arm64" \
-  --dart-define=APP_VERSION="$ver" \
-  --dart-define=GIT_COMMIT="$gc"
+echo "Preparing temp build dir: $BUILD_DIR"
 
-flutter build apk --release --split-per-abi \
-  --target-platform="android-x64" \
-  --dart-define=APP_VERSION="$ver" \
-  --dart-define=GIT_COMMIT="$gc"
+[ -d "$BUILD_DIR" ] && rm -rf "$BUILD_DIR"
+
+set -x
+
+export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
+
+mkdir "$BUILD_DIR"
+
+cp -r android "$BUILD_DIR"
+cp -r assets "$BUILD_DIR"
+cp -r lib "$BUILD_DIR"
+cp pubspec.lock "$BUILD_DIR"
+cp pubspec.yaml "$BUILD_DIR"
+
+# maybe not needed
+cp analysis_options.yaml "$BUILD_DIR"
+cp devtools_options.yaml "$BUILD_DIR"
+cp flutter_launcher_icons.yaml "$BUILD_DIR"
+
+cd "$BUILD_DIR"
+
+export PUB_CACHE=$(pwd)/.pub-cache
+
+[ -d "$OLDPWD/$OUT_DIR" ] && rm -rf "$OLDPWD/$OUT_DIR"
+mkdir -p "$OLDPWD/$OUT_DIR"
+
+flutter clean
+flutter pub get
+
+sed -i -e 's/-Wl,/-Wl,--build-id=none,/' ${PUB_CACHE}/hosted/*/jni-*/src/CMakeLists.txt
+
+# 1
+flutter build apk \
+    --release \
+    --split-per-abi \
+    --target-platform="android-arm" \
+    --dart-define="APP_VERSION=$version" \
+    --dart-define=GIT_COMMIT="$gc"
+
+cp build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk \
+   "$OLDPWD/$bd/$n-v$version-armeabi-v7a.apk"
+
+# 2
+flutter build apk \
+    --release \
+    --split-per-abi \
+    --target-platform="android-arm64" \
+    --dart-define="APP_VERSION=$version" \
+    --dart-define=GIT_COMMIT="$gc"
 
 
-cp 'build/app/outputs/flutter-apk/app-arm64-v8a-release.apk' "${pre}_arm64-v8a.apk"
-cp 'build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk' "${pre}_armeabi-v7a.apk"
-cp 'build/app/outputs/flutter-apk/app-x86_64-release.apk' "${pre}_x86_64.apk"
+cp build/app/outputs/flutter-apk/app-arm64-v8a-release.apk \
+   "$OLDPWD/$bd/$n-v$version-arm64-v8a.apk"
+
+# 3
+flutter build apk \
+    --release \
+    --split-per-abi \
+    --target-platform="android-x64" \
+    --dart-define="APP_VERSION=$version" \
+    --dart-define=GIT_COMMIT="$gc"
+
+cp build/app/outputs/flutter-apk/app-x86_64-release.apk \
+   "$OLDPWD/$bd/$n-v$version-x86_64.apk"
 
 flutter build apk --release \
   --dart-define=APP_VERSION="$ver" \
-  --dart-define=GIT_COMMIT="$gc" \
+  --dart-define=GIT_COMMIT="$gc"
 
-cp 'build/app/outputs/flutter-apk/app-release.apk' "${pre}_universal.apk"
+cp 'build/app/outputs/flutter-apk/app-release.apk' \
+    "$OLDPWD/$bd/$n-v$version-universal.apk"
 
-# linux
-flutter build linux --release \
-  --dart-define=APP_VERSION="$ver" \
-  --dart-define=GIT_COMMIT="$gc" \
+cd -
+
+echo "Done: APKs copied to $OLDPWD/$OUT_DIR/"
 
 
 linux_zip="${n}_${ver}_linux.zip"

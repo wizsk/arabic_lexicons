0c5bb65b88b07823519815e5406a6f159fc0b3b6
Sakib <sakibul706@gmail.com>
2026-08-02 23:21:19 +0600

    rel.sh

--- b/rel.sh
+++ b/rel.sh
@@ -27,32 +27,32 @@ echo "Version: $version"
 
 set -x
 
-# export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
+export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
 
 # 1
-# flutter build apk \
-#     --release \
-#     --split-per-abi \
-#     --dart-define="APP_VERSION=$version" \
-#     --dart-define=GIT_COMMIT="$gc"
-#     # --target-platform="android-arm" \
-#
-# cp build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk \
-#    "$bd/$n-v$version-armeabi-v7a.apk"
-#
-#
-# cp build/app/outputs/flutter-apk/app-arm64-v8a-release.apk \
-#    "$bd/$n-v$version-arm64-v8a.apk"
-#
-# cp build/app/outputs/flutter-apk/app-x86_64-release.apk \
-#    "$bd/$n-v$version-x86_64.apk"
-#
-# flutter build apk --release \
-#   --dart-define=APP_VERSION="$ver" \
-#   --dart-define=GIT_COMMIT="$gc"
-#
-# cp 'build/app/outputs/flutter-apk/app-release.apk' \
-#     "$bd/$n-v$version-universal.apk"
+flutter build apk \
+    --release \
+    --split-per-abi \
+    --dart-define="APP_VERSION=$version" \
+    --dart-define=GIT_COMMIT="$gc"
+    # --target-platform="android-arm" \
+
+cp build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk \
+   "$bd/$n-v$version-armeabi-v7a.apk"
+
+
+cp build/app/outputs/flutter-apk/app-arm64-v8a-release.apk \
+   "$bd/$n-v$version-arm64-v8a.apk"
+
+cp build/app/outputs/flutter-apk/app-x86_64-release.apk \
+   "$bd/$n-v$version-x86_64.apk"
+
+flutter build apk --release \
+  --dart-define=APP_VERSION="$ver" \
+  --dart-define=GIT_COMMIT="$gc"
+
+cp 'build/app/outputs/flutter-apk/app-release.apk' \
+    "$bd/$n-v$version-universal.apk"
 
 echo "Done: APKs copied"
 

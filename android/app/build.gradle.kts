17be44b240d9fd3fdda8fb1e18154e11eeace185
Sakib <sakibul706@gmail.com>
2026-08-20 23:16:04 +0600

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,6 +26,12 @@ android {
         resValues = true
     }
 
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17

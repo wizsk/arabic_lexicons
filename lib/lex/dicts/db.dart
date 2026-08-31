c6ecf729adf55a8ebf23e4a42040a402fde61b69
Sakib <sakibul706@gmail.com>
2026-08-31 15:52:34 +0600

    Update db.dart

--- lib/lex/dicts/db.dart
+++ lib/lex/dicts/db.dart
@@ -9,7 +9,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-/// db is saved in cache directory
+/// db is saved in data directory
 const String dbFileName = 'arabic_lexicons_db_v3.sqlite';
 
 class DbRow {
